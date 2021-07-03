d8dcbbb2f72ab8850e3e58bcb119fb98a29326fb
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2021-07-03 16:30:23 +0300

    Use gcd function to validate prime number

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from math import sqrt
+from math import gcd
 import random
 
 
-MIN_NUM = 1
+MIN_NUM = -100
 MAX_NUM = 500
 
 
@@ -11,24 +11,16 @@ task_description = ('Answer "yes" if given number is prime. '
                     'Otherwise answer "no".')
 
 
-def get_prime_numbers(max_number=500):
-    first_prime_number = 2
-    prime_numbers = [first_prime_number]
-
-    # check only odd numbers
-    for number in range(3, max_number + 1, 2):
-        is_prime = True
-        # check divisibility up to square of the number
-        for i in range(2, int(sqrt(number)) + 1):
-            if number % i == 0:
-                is_prime = False
-        if is_prime:
-            prime_numbers.append(number)
-    return prime_numbers
-
-
 def get_task():
     number = random.randint(MIN_NUM, MAX_NUM)
-    prime_numbers = get_prime_numbers()
-    answer = 'yes' if number in prime_numbers else 'no'
+
+    is_prime = True
+    if number > 1:
+        for i in range(2, number):
+            if gcd(i, number) != 1:
+                is_prime = False
+    else:
+        is_prime = False
+
+    answer = 'yes' if is_prime else 'no'
     return number, answer
