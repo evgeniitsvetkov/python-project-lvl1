695d3fcc795b5e1ed031f6863356770d51db1b4e
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2021-07-12 10:34:59 +0300

    Add else clause on while statement. Minor logic and naming update

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,32 +4,25 @@ import prompt
 GUESSES_TO_WIN = 3
 
 
-def welcome_user():
+def play_game(task_description, get_task):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
+    print(f'Hello, {name}!\n{task_description}')
 
-
-def play_game(task_description, get_task):
-    name = welcome_user()
-
-    print(task_description)
-
-    guesses_in_a_row = 0
-    while True:
-        question, answer = get_task()
+    tries = GUESSES_TO_WIN
+    while tries:
+        question, task_answer = get_task()
         print(f'Question: {question}')
 
-        user_input = prompt.string('Your answer: ')
-        if user_input == answer:
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == task_answer:
             print('Correct!')
-            guesses_in_a_row += 1
-            if guesses_in_a_row == GUESSES_TO_WIN:
-                print(f'Congratulations, {name}!')
-                break
+            tries -= 1
         else:
-            print(f"'{user_input}' is wrong answer ;(. "
-                  f"Correct answer was '{answer}'.\n"
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{task_answer}'.\n"
                   f"Let's try again, {name}!")
             break
+
+    else:
+        print(f'Congratulations, {name}!')
